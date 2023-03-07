ef6cfd43fb845f3f90d9cae5b84978a5b36d0444
giapdoan01 <gdoan012@gmail.com>
2023-03-07 09:51:32 +0700

    giap

---
+++ Chuong3/11_BTOTfor3-32.py
@@ -0,0 +1,4 @@
+n=17
+for i in range(1,n+1,2):
+    print(((n-i)//2)*" ",i*"*",sep="")
+print()
--- Chuong3/BTCN/11_BTCN3-7.py
+++ Chuong3/BTCN/11_BTCN3-7.py
@@ -1,9 +1,29 @@
-# while True:
-#  n=int(input())
-#  a=1
-#  if n<0:
-#     break
-#  else:
-#    for i in range(1,n+1):
-#         a=a*i
-#  print(a)
+# a
+n=int(input())
+i=1
+giaithua=1
+while n==0 :
+    print('1')
+    n=int(input())
+while n>0 and i<=n:
+    giaithua*=i
+    i+=1  
+print(giaithua)
+n=int(input())
+while n<0:
+    break 
+#b
+n=int(input())
+while n>=0:
+    i=1
+    giaithua=1
+    if n>0:
+        for i in range (1,n+1):
+            giaithua*=i
+        print(giaithua)
+        n=int(input())
+    elif n<0:
+        break
+    else:
+        print('1')
+        n=int(input())
